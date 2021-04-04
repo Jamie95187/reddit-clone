cdd434cf13ea6de1fe3c3dcf03b4acffa6141cea
Jamie <jamiewong63@googlemail.com>
2021-04-04 19:58:27 +0100

    setting up database and mikro orm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world!");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Posts";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: 'lireddit',
+    type: 'postgresql',
+    debug: !__prod__
+  });
+
+  const post = orm.em.create(Post, {title: 'my first post'});
+  await orm.em.persistAndFlush(post);
+
+};
+
+main();
