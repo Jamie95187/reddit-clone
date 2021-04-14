5ea30bf6d70946f0fb7b88f449f02cabf8c7e7e3
Jamie <jamiewong63@googlemail.com>
2021-04-14 23:25:54 +0100

    init project

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import "reflect-metadata";
-import { createConnection, QueryRunner } from "typeorm";
-
-async () => {
-     const connection = await createConnection();
-     const queryRunner: QueryRunner = connection.createQueryRunner();
-
-     await queryRunner.connect();
-};
