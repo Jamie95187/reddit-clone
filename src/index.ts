7a4f7c76f8b7f252b19b1e12c9d9b6dbf2e183d8
Jamie Wong <jamiewong63@googlemail.com>
2021-04-23 23:50:43 +0100

    found error comes from post entity and importing typegraphql

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-// import "reflect-metadata";
+import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {Post} from "./entity/Post";
-// import express from 'express';
-// import {ApolloServer} from 'apollo-server-express';
-// import {buildSchema} from 'type-graphql';
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
 
 // Unlike mikroOrm, the createConnection function automatically finds the ormconfig.json file as long as it is
 // near the package.json (root directory)
@@ -13,26 +13,26 @@ createConnection().then(async connection => {
     console.log("Inserting a new post into the database...");
     const post = new Post();
     post.title = "First Post";
-    // await connection.manager.save(post);
-    // console.log("Saved a new post with id: " + post.id);
+    await connection.manager.save(post);
+    console.log("Saved a new post with id: " + post.id);
 
     console.log("Loading posts from the database...");
     const posts = await connection.manager.find(Post);
     console.log("Loaded users: ", posts);
 
-    // const app = express();
-    // const apolloServer = new ApolloServer({
-    //   schema: await buildSchema({
-    //     resolvers: [HelloResolver, PostResolver],
-    //     validation: false
-    //   }),
-    //   context: () => ({ posts })
-    // });
+    const app = express();
+    const apolloServer = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [HelloResolver, PostResolver],
+        validation: false
+      }),
+      context: () => ({ posts })
+    });
 
-    // apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({ app });
 
-    // app.listen(8080, () => {
-    //   console.log('server started on localhost:8080')
-    // })
+    app.listen(8080, () => {
+      console.log('server started on localhost:8080')
+    })
 
 }).catch(error => console.log(error));
