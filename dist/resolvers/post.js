0fbe723daa89bba4263eda18ef01fe23ac7d8f7c
Jamie <jamiewong63@googlemail.com>
2021-05-03 05:18:42 +0100

    can make query for singular post

--- dist/resolvers/post.js
+++ dist/resolvers/post.js
@@ -1,11 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
-const { Resolver, Query, Ctx } = require("type-graphql");
+const { Resolver, Query, Ctx, Arg, Int } = require("type-graphql");
 const Post_1 = require("../entity/Post");
 let PostResolver = class PostResolver {
     posts({ cm }) {
         return cm.find(Post_1.Post, {});
+    }
+    post(id, { cm }) {
+        return cm.findOne(Post_1.Post, { id });
     }
 };
 tslib_1.__decorate([
@@ -15,6 +18,14 @@ tslib_1.__decorate([
     tslib_1.__metadata("design:paramtypes", [Object]),
     tslib_1.__metadata("design:returntype", Promise)
 ], PostResolver.prototype, "posts", null);
+tslib_1.__decorate([
+    Query(() => Post_1.Post, { nullable: true }),
+    tslib_1.__param(0, Arg('id', () => Int)),
+    tslib_1.__param(1, Ctx()),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", [Number, Object]),
+    tslib_1.__metadata("design:returntype", Promise)
+], PostResolver.prototype, "post", null);
 PostResolver = tslib_1.__decorate([
     Resolver()
 ], PostResolver);
--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,4 +1,4 @@
-const { Resolver, Query, Ctx } = require("type-graphql");
+const { Resolver, Query, Ctx, Arg, Int } = require("type-graphql");
 import { Post } from "../entity/Post";
 import { MyContext } from '../types';
 
@@ -7,5 +7,11 @@ export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { cm }: MyContext): Promise<Post[]> {
     return cm.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg('id', () => Int) id: number,
+       @Ctx() { cm }: MyContext): Promise<Post | null> {
+    return cm.findOne(Post, {id});
   }
 }
