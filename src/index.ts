ffc69eb83c8536af59ff59d4c2b7e859fe7afb08
Jamie <jamiewong63@googlemail.com>
2021-05-04 10:40:13 +0100

    mutations do not work in graphql playground 'message : Schema is not configured for mutations.'

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,6 @@ const { ApolloServer } = require('apollo-server-express');
 
 createConnection().then(async connection => {
 
-    console.log("Inserting a new post into the database...");
-    const post = new Post();
-    post.title = "First Post";
-    await connection.manager.save(post);
-    console.log("Saved a new post with id: " + post.id);
-
     console.log("Loading posts from the database...");
     const posts = await connection.manager.find(Post);
     console.log("Loaded users: ", posts);
@@ -26,7 +20,8 @@ createConnection().then(async connection => {
 
     const apolloServer = new ApolloServer({
       schema: await buildSchema({
-        resolvers: [HelloResolver, PostResolver],
+        resolvers: [HelloResolver, PostResolver],,
+        emitSchemaFile: true,
         validate: false
       }),
       context: () => ({ cm: connection.manager })
--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,9 +1,12 @@
-const { Resolver, Query, Ctx, Arg, Int } = require("type-graphql");
+const { Resolver, Mutation, Query, Ctx, Arg, Int } = require("type-graphql");
 import { Post } from "../entity/Post";
 import { MyContext } from '../types';
 
 @Resolver()
 export class PostResolver {
+
+  // Queries are used for getting data
+
   @Query(() => [Post])
   posts(@Ctx() { cm }: MyContext): Promise<Post[]> {
     return cm.find(Post, {});
@@ -13,5 +16,18 @@ export class PostResolver {
   post(@Arg('id', () => Int) id: number,
        @Ctx() { cm }: MyContext): Promise<Post | null> {
     return cm.findOne(Post, {id});
+  }
+
+  // Mutations are for updating, inserting and deleting
+
+  @Mutation(() => Post)
+  async createPost(@Arg("title", () => String) title: string,
+       @Ctx() { cm }: MyContext): Promise<Post> {
+         console.log("Inserting a new post into the database...");
+         const post = new Post();
+         post.title = "First Post";
+         await connection.manager.save(post);
+         console.log("Saved a new post with id: " + post.id);
+         return post;
   }
 }
