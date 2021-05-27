c0844b33a1636983b6a283122ff895f5a6234e03
Jamie <jamiewong63@googlemail.com>
2021-05-27 23:11:37 +0100

    mock request session

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { Session } from 'express-session';
 
 export type MyContext = {
-  req: Request & {session?: Session & { userId?: Number }};
+  // req: Request & {session?: Session & { userId?: Number }};
+  req: Request { session: Session & userId?: 6 };
   res: Response;
 }
