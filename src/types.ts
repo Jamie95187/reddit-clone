0c35189d2c7d5ccda30459212314a98036d39992
Jamie <jamiewong63@googlemail.com>
2021-06-01 22:05:53 +0100

    requst can store any object

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { Session } from 'express-session';
 
 export type MyContext = {
+  // Request can store anything, can even store whole user Object
   req: Request & {session?: Session & { userId?: Number }};
   // Mock Request
   // req: Request & { session?: Session & { userId?: 6 } };
